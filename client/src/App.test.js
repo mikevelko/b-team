5273871c3e6500f997c14836f260aa742114b42d
mikevelko <44818580+mikevelko@users.noreply.github.com>
2021-04-09 12:41:31 +0200

    Client app tests (#136)
    * Create client-node.js.yml
    
    * tests: Removed failing tests
    
    * client-tests
    
    Co-authored-by: Gloomwidow <michalmachnio.mm@gmail.com>

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import "@testing-library/jest-dom/extend-expect";
+import MainPage from './components/MainPage';
+import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Hotels from './components/Hotels';
+import MyReservations from './components/MyReservations';
+import LoginPage from './components/LoginPage';
 
-test('renders learn react link', () => {
+
+test('bookly client label logo test', () => {
   render(<App />);
-  const linkElement = screen.getByText("Bookly client");
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Bookly client")).toBeInTheDocument();
 });
+
+test('button my profile test', () => {
+  render(<Button variant="contained" color="primary" size="large" data-testid="my-profile">My profile</Button>);
+  expect(screen.getByText("My profile")).toBeInTheDocument();
+});
+
+
+test('login component test', () => {
+  render(<App />);
+  expect(screen.getByText("Login")).toBeInTheDocument();
+});
+
+test('login component test', () => {
+  render(<App />);
+  expect(screen.getByText("username")).toBeInTheDocument();
+});
+
+test('login component test', () => {
+  render(<App />);
+  expect(screen.getByText("password")).toBeInTheDocument();
+});
+
+test('main page test', () => {
+  render(<Router><PrivateRoute authed={true} exact path='/home' component={MainPage}/></Router>);
+  expect(screen.getByText("Hotels")).toBeInTheDocument();
+});
+
+test('main page test', () => {
+  render(<Router><PrivateRoute authed={true} exact path='/home' component={MainPage}/></Router>);
+  expect(screen.getByText("My profile")).toBeInTheDocument();
+});
+
+test('hotels view test', () => {
+  render(<Router><PrivateRoute authed={true} exact path='/hotels' component={Hotels}/></Router>);
+  expect(screen.getByText("Search")).toBeInTheDocument();
+});
+
+test('my reservtaions view test', () => {
+  render(<Router><PrivateRoute authed={true} exact path='/client/reservations' component={MyReservations} /></Router>);
+  expect(screen.getByText("My reservations:")).toBeInTheDocument();
+});
+
