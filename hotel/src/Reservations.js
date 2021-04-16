e7601fe134036cfd27c74f530966335b9524e197
Aliaksei <52509435+FlowerAlex@users.noreply.github.com>
2021-04-17 01:43:40 +0200

    Complete view for reservations (#191)
    * EditOffer in progress
    
    * complete edit offer view
    
    * complete view for reservations

--- hotel/src/Reservations.js
+++ hotel/src/Reservations.js
@@ -1,8 +1,83 @@
-import React from 'react';
+import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Reservations.css";
+
+const useStyles = makeStyles((theme) => ({
+  title:{
+    marginTop:10,
+    fontSize: 25
+  },
+}));
 
 function Reservations() {
+  const classes = useStyles();
+
   return (
-    <h1>Reservations</h1>
+    <>
+
+      <Grid className="Reservations">
+        <Typography className={classes.title}>
+          Reservations
+        </Typography>
+        <Box className="ReservationRow">
+          <Box>
+            <Typography>Date from:[Date]</Typography>
+            <Typography>Date to:[Date]</Typography>
+          </Box>
+          <Box>
+            <Typography>Count of adults:[count]</Typography>
+            <Typography>Count of children:[count]</Typography>
+          </Box>
+          <Box>
+            <Typography>Room numbers:</Typography>
+            <Typography>[room number list]</Typography>
+          </Box>
+        </Box>
+        <Box className="ReservationRow">
+          <Box>
+            <Typography>Date from:[Date]</Typography>
+            <Typography>Date to:[Date]</Typography>
+          </Box>
+          <Box>
+            <Typography>Count of adults:[count]</Typography>
+            <Typography>Count of children:[count]</Typography>
+          </Box>
+          <Box>
+            <Typography>Room numbers:</Typography>
+            <Typography>[room number list]</Typography>
+          </Box>
+        </Box>
+        <Box className="ReservationRow">
+          <Box>
+            <Typography>Date from:[Date]</Typography>
+            <Typography>Date to:[Date]</Typography>
+          </Box>
+          <Box>
+            <Typography>Count of adults:[count]</Typography>
+            <Typography>Count of children:[count]</Typography>
+          </Box>
+          <Box>
+            <Typography>Room numbers:</Typography>
+            <Typography>[room number list]</Typography>
+          </Box>
+        </Box>
+        <Box className="ReservationRow">
+          <Box>
+            <Typography>Date from:[Date]</Typography>
+            <Typography>Date to:[Date]</Typography>
+          </Box>
+          <Box>
+            <Typography>Count of adults:[count]</Typography>
+            <Typography>Count of children:[count]</Typography>
+          </Box>
+          <Box>
+            <Typography>Room numbers:</Typography>
+            <Typography>[room number list]</Typography>
+          </Box>
+        </Box>
+      </Grid>
+    </>
   );
 }
 
