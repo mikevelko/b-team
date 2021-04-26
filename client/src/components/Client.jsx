e7515e02bff067d18eebc14a3a535d9c863c513a
mikevelko <44818580+mikevelko@users.noreply.github.com>
2021-04-27 01:39:29 +0200

    Fix/refreshing client info after update (#210)

--- client/src/components/Client.jsx
+++ client/src/components/Client.jsx
@@ -40,8 +40,7 @@ function Client() {
     function isValidEmailAddress(address) {
         return !!address.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/);
     }
-    function isValidUsername(username) 
-    {
+    function isValidUsername(username) {
         return username.length > 2;
     }
 
@@ -61,22 +60,22 @@ function Client() {
             email: email
         };
         const headers = {
-            'accept': 'application/json', 
+            'accept': 'application/json',
             'x-session-token': window.localStorage.getItem("token"),
             'Content-Type': 'application/json'
-        }; 
+        };
         axios.patch('/api-client/client', body, { headers })
             .then(response => {
                 console.log(response);
                 setIsEditing(false);
+                fetchItems();
             })
             .catch(error => {
                 console.error('There was an error!', error.response);
             });
-
-
         
     }
+
     function EditInformation() {
         setIsEditing(true);
     }
@@ -90,14 +89,14 @@ function Client() {
             <div className="nav-inputs">
                 <ul>
                     <li className="li">
-                        <TextField required id="outlined-basic" label="Name" variant="outlined" disabled={true} value={name}/>
+                        <TextField required id="outlined-basic" label="Name" variant="outlined" disabled={true} value={name} />
                     </li>
                     <li className="li">
-                        <TextField required id="outlined-basic" label="Surname" variant="outlined" disabled={true} value={surname}/>
+                        <TextField required id="outlined-basic" label="Surname" variant="outlined" disabled={true} value={surname} />
                     </li>
                     <li className="li">
-                        <TextField required id="outlined-basic" label="username" variant="outlined" disabled={!IsEditing} value={username} onChange={handleChangeUsername} 
-                        error={!isValidUsername(username)} helperText={!isValidUsername(username) ? 'username is too short' : ' '}/>
+                        <TextField required id="outlined-basic" label="username" variant="outlined" disabled={!IsEditing} value={username} onChange={handleChangeUsername}
+                            error={!isValidUsername(username)} helperText={!isValidUsername(username) ? 'username is too short' : ' '} />
                     </li>
                     <li className="li">
                         <TextField required id="outlined-basic" label="email" variant="outlined" disabled={!IsEditing} value={email} onChange={handleChangeEmail}
