5a780cd7fba5e61eead0ca0e95168f3a4de9a13b
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2021-05-19 01:51:44 +0200

    fix: wrong images push setup

--- deploy/push-server-images.sh
+++ deploy/push-server-images.sh
@@ -7,7 +7,7 @@ COMMIT_TAG=$(git rev-parse --short HEAD)
 
 for service in auth-service hotel-service offer-service room-service user-service
 do
-  IMAGE_ID="${ACR_URL}/${REPO_NAME}-${service}:${COMMIT_TAG}"
+  IMAGE_ID="${REGISTRY_URL}/${REPO_NAME}-${service}:${COMMIT_TAG}"
   echo "building $service with image ID=${IMAGE_ID}"
   docker build --build-arg SERVICE_NAME=${service} -t ${IMAGE_ID}  "${PROJECT_ROOT}/server"
   docker push "${IMAGE_ID}"
