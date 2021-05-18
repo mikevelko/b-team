7c187612ab564f66304b238d3e2c5a00883ec33e
areknoster <38364298+areknoster@users.noreply.github.com>
2021-05-19 01:42:42 +0200

    feat: add pushing of server images (#269)

---
+++ deploy/push-server-images.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+REPO_NAME=$(basename ${PROJECT_ROOT})
+COMMIT_TAG=$(git rev-parse --short HEAD)
+
+
+for service in auth-service hotel-service offer-service room-service user-service
+do
+  IMAGE_ID="${ACR_URL}/${REPO_NAME}-${service}:${COMMIT_TAG}"
+  echo "building $service with image ID=${IMAGE_ID}"
+  docker build --build-arg SERVICE_NAME=${service} -t ${IMAGE_ID}  "${PROJECT_ROOT}/server"
+  docker push "${IMAGE_ID}"
+done
